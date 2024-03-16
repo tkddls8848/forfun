dd037e9c0985efd4d36c95f786f3f0be6a1ac390
tkddls8848 <tkddls8848@gmail.com>
2024-03-16 17:39:16 +0900

    k8s

--- 03. k8s/master_node/jenkins.sh
+++ 03. k8s/master_node/jenkins.sh
@@ -11,9 +11,7 @@ kubectl create namespace devops-tools
 sudo sed -i 's/- worker-node01/- k8s-worker1/' volume.yaml
 
 # deploy jenkins service
-kubectl create -f volume.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f .
 
 # get initial admin password
 # kubectl exec -it (pod name) cat /var/jenkins_home/secrets/initialAdminPassword -n devops-tools
--- 03. k8s/nfs/nfs_node.sh
+++ 03. k8s/nfs/nfs_node.sh
@@ -11,6 +11,6 @@ sudo yum install -y yum-utils vim
 
 # enroll share folder
 sudo bash -c 'cat << EOF > /etc/exports
-/nfs 192.168.1.0/8(rw,sync,no_subtree_check,no_root_squash)
+/nfs *(rw,sync,no_subtree_check,no_root_squash)
 EOF'
 sudo systemctl restart nfs-server
