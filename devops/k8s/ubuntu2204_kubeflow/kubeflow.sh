b6c54a3c40504f22ca50060a29344ec794997073
tkddls8848 <tkddls8848@gmail.com>
2024-07-18 00:07:06 +0900

    kubeflow init

---
+++ devops/k8s/ubuntu2204_kubeflow/kubeflow.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+git clone https://github.com/kubernetes-sigs/nfs-subdir-external-provisioner
+cd ~/nfs-subdir-external-provisioner/deploy/
+sudo rm deployment.yaml
+sudo bash -c 'cat << EOF > ./deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nfs-client-provisioner
+  labels:
+    app: nfs-client-provisioner
+  namespace: default
+spec:
+  replicas: 1
+  strategy:
+    type: Recreate
+  selector:
+    matchLabels:
+      app: nfs-client-provisioner
+  template:
+    metadata:
+      labels:
+        app: nfs-client-provisioner
+    spec:
+      serviceAccountName: nfs-client-provisioner
+      containers:
+        - name: nfs-client-provisioner
+          image: registry.k8s.io/sig-storage/nfs-subdir-external-provisioner:v4.0.2
+          volumeMounts:
+            - name: nfs-client-root
+              mountPath: /persistentvolumes
+          env:
+            - name: PROVISIONER_NAME
+              value: k8s-sigs.io/nfs-subdir-external-provisioner
+            - name: NFS_SERVER
+              value: 192.168.55.100
+            - name: NFS_PATH
+              value: /srv/nfs-volume
+      volumes:
+        - name: nfs-client-root
+          nfs:
+            server: 192.168.55.100
+            path: /srv/nfs-volume
+EOF'
+kubectl apply -f .
+
+cd ~
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+sudo mv kustomize /usr/local/bin
+
+git clone https://github.com/kubeflow/manifests.git
+cd manifests
+while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
