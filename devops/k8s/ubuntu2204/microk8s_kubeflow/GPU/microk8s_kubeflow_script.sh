360371f4eacf29a2b13a48253e9cdb9df651594d
tkddls8848 <tkddls8848@naver.com>
2024-10-01 23:34:46 +0900

    devops => microk8s in host ubuntu OS

--- devops/k8s/ubuntu2204/microk8s_kubeflow/GPU/microk8s_kubeflow_script.sh
+++ devops/k8s/ubuntu2204/microk8s_kubeflow/GPU/microk8s_kubeflow_script.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/bash
+#run script in ubuntu OS
+
+# swapoff -a to disable swapping
+sudo swapoff -a
+sudo sed -e '/swap/s/^/#/' -i /etc/fstab
+
+## install microk8s
+sudo snap install microk8s --classic --channel=1.29/stable
+
+## add user group for use microk8s
+mkdir -p ~/.kube
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+newgrp microk8s ## restart session required
+
+## install addons
+#microk8s status --wait-ready
+sudo microk8s enable dns hostpath-storage metallb:10.64.140.43-10.64.140.49 rbac
 
 ## install juju
 sudo snap install juju  --classic --channel=3.4/stable
