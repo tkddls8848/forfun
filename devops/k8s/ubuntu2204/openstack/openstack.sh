45868989fcb8e8b5129496caf24948e21bab8e33
tkddls8848 <tkddls8848@gmail.com>
2025-02-01 18:10:10 +0900

    AI automations

--- devops/k8s/ubuntu2204/openstack/openstack.sh
+++ devops/k8s/ubuntu2204/openstack/openstack.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
 
-## https://gam1532.tistory.com/30
 ## add stack user
-sudo adduser --disabled-password stack
-echo "stack:stack" | sudo chpasswd
-sudo su -
-echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo su stack
-## clone devstack
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+sudo -u stack -i
+
+## clone devstack git repo
 cd ~/
 sudo apt-get install git
 git clone https://git.openstack.org/openstack-dev/devstack
 cd ~/devstack
-## config local ip info in local.conf
+
+## config info in local.conf
 sudo cp ./samples/local.conf ./
 sed -i "s/#HOST_IP=w.x.y.z/HOST_IP=\$(hostname -I | awk '{print \$2}')/" ./local.conf
+sed -i "s/ADMIN_PASSWORD=nomoresecret/ADMIN_PASSWORD=admin/" ./local.conf
+
 ## exec install openstack script
 ./stack.sh
